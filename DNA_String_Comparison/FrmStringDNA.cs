95f5d576feb663d710d19861df913aecd99d4326
Alex Almeida <alex.aldr@gmail.com>
2018-10-14 16:34:41 -0300

    Update DNA_String_Comparison/FrmStringDNA.cs

--- DNA_String_Comparison/FrmStringDNA.cs
+++ DNA_String_Comparison/FrmStringDNA.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace DNA_String_Comparison
 {
@@ -39,7 +40,7 @@ namespace DNA_String_Comparison
                             // Read the stream to a string
                             dna = sr.ReadToEnd();
                             label.Text = openFileDialog.FileName;
-                            MessageBox.Show("Sequência carregada com sucesso!", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            //MessageBox.Show("Sequência carregada com sucesso!", "Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
                             //MessageBox.Show($"{dna.Length}");
                         }
                     }
@@ -76,32 +77,91 @@ namespace DNA_String_Comparison
 
         private void btnDNAComparison_Click(object sender, EventArgs e)
         {
+            rTxtComparisonResult1.Text = "";
+            rTxtComparisonResult2.Text = "";
             int count = 0;
+            int i = 0;
             Cursor.Current = Cursors.WaitCursor;
-            MessageBox.Show("Aguarde o término da comparação!\n\nO programa pode apresentar lentidão no processo.\n\n\nClique em OK para continuar!", "Aguarde!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //MessageBox.Show("Aguarde o término da comparação!\n\nO programa pode apresentar lentidão no processo.\n\n\nClique em OK para continuar!", "Aguarde!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            var stopwatch = new Stopwatch();
+
+            Byte[] bArray01 = Encoding.UTF8.GetBytes(dna01);
+            Byte[] bArray02 = Encoding.UTF8.GetBytes(dna02);
+
+
             if (dna01.Length == dna02.Length)
             {
-                for (int i = 0; i < dna01.Length; i++)
+                stopwatch.Start();
+                //for (int i = 0; i < dna01.Length; i++)
+                //foreach(char dna in dna01)
+                foreach (byte bElements in bArray01)
                 {
-                    if (dna01[i] != dna02[i])
+                    if (bArray01[i] == bArray02[i])                 //13.4 seconds
+                    //if(Char.ToUpper(dna01[i]).CompareTo(Char.ToUpper(dna01[i]))==0)     //13.6 seconds
+                    //if (string.CompareOrdinal($"{dna01[i]}", $"{dna02[i]}") == 0)       //13.8 seconds
+                    //if (dna01[i].Equals(dna02[i]))                                      //14.1 seconds
+                    //if (Char.ToUpper(dna01[i]).Equals(Char.ToUpper(dna02[i])))          //14.2 seconds
                     {
-                        count++;
-                        rTxtComparisonResult1.AppendText($"{dna01[i]}", Color.Red);
-                        rTxtComparisonResult2.AppendText($"{dna02[i]}", Color.Red);
+                        //rTxtComparisonResult1.AppendText($"{(char)bArray01[i]}", Color.Gray);
+                        //rTxtComparisonResult2.AppendText($"{(char)bArray02[i]}", Color.Gray);
                     }
                     else
                     {
-                        rTxtComparisonResult1.AppendText($"{dna01[i]}", Color.Gray);
-                        rTxtComparisonResult2.AppendText($"{dna02[i]}", Color.Gray);
+                        count++;
+                        //rTxtComparisonResult1.AppendText($"{(char)bArray01[i]}", Color.Red);
+                        //rTxtComparisonResult2.AppendText($"{(char)bArray02[i]}", Color.Red);
                     }
+                    i++;
                 }
-                lblResult.Text = $"As sequências possuem {count} base(s) diferente(s)!\n\nHá xx subsequências iguais com mais de xx bases.";
+                stopwatch.Stop();
+                lblResult.Text = $"As sequências possuem {count} base(s) diferente(s)!"
+                               + $"\n\nHá xx subsequências iguais com mais de xx bases."
+                               + $"\n\nA operação demorou {stopwatch.Elapsed.TotalSeconds} segundos para ser concluída!";
                 Cursor.Current = Cursors.Arrow;
             }
             else
             {
                 MessageBox.Show("Operação cancelada!\nAs sequências de DNA possuem comprimentos diferentes!", "Cancelado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
+
+            /*
+            if (dna01.Length == dna02.Length)
+            {
+                stopwatch.Start();
+                for (int i = 0; i < dna01.Length; i++)
+                //foreach(char dna in dna01)
+                {
+                    if (char.ToUpper(dna01[i]) == char.ToUpper(dna02[i]))                 //13.4 seconds
+                    //if(Char.ToUpper(dna01[i]).CompareTo(Char.ToUpper(dna01[i]))==0)     //13.6 seconds
+                    //if (string.CompareOrdinal($"{dna01[i]}", $"{dna02[i]}") == 0)       //13.8 seconds
+                    //if (dna01[i].Equals(dna02[i]))                                      //14.1 seconds
+                    //if (Char.ToUpper(dna01[i]).Equals(Char.ToUpper(dna02[i])))          //14.2 seconds
+                    {
+                        rTxtComparisonResult1.AppendText($"{dna01[i]}", Color.Gray);
+                        rTxtComparisonResult2.AppendText($"{dna02[i]}", Color.Gray);
+                    }
+                    else
+                    {
+                        count++;
+                        rTxtComparisonResult1.AppendText($"{dna01[i]}", Color.Red);
+                        rTxtComparisonResult2.AppendText($"{dna02[i]}", Color.Red);
+                    }
+                    i++;
+                }
+                stopwatch.Stop();
+                lblResult.Text = $"As sequências possuem {count} base(s) diferente(s)!"
+                               + $"\n\nHá xx subsequências iguais com mais de xx bases."
+                               + $"\n\nA operação demorou {stopwatch.Elapsed.TotalSeconds} segundos para ser concluída!";
+                Cursor.Current = Cursors.Arrow;
+            }
+            else
+            {
+                MessageBox.Show("Operação cancelada!\nAs sequências de DNA possuem comprimentos diferentes!", "Cancelado!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            */
+
+
 
             /*
             // Hold the index and the discrepancy
